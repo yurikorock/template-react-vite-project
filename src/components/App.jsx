0d2a0a96a87f8a6923134ee6c6691b87e07013ef
Yura Shaplavsky <yurikorock777@gmail.com>
2025-03-21 17:04:21 +0200

    create structure project

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+import "./App.css";
+import Product from "./Product";
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+// src/App.jsx
+
+// src/components/App.jsx
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+    </div>
+  );
+}
